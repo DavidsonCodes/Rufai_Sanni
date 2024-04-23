cbb9a28514687b062b848d1ec15235479e46f10a
Rufai Sanni <sruffy07@gmail.com>
2024-04-23 10:48:36 +0100

    updated the annotation for phoneNumber field in the AccountUser model

--- Banking_Application_Development/src/main/java/com/example/Banking_Application_Developement/model/AccountUser.java
+++ Banking_Application_Development/src/main/java/com/example/Banking_Application_Developement/model/AccountUser.java
@@ -3,12 +3,15 @@ package com.example.Banking_Application_Developement.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import org.hibernate.annotations.IdGeneratorType;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.hateoas.RepresentationModel;
 
 
 @Entity
+@Table(name = "account_user_table")
 //@Table(name="account_user", uniqueConstraints = @UniqueConstraint(columnNames = {"email", "phone_number"}))
 public class AccountUser extends RepresentationModel<AccountUser> {
 
@@ -29,12 +32,28 @@ public class AccountUser extends RepresentationModel<AccountUser> {
 
     @Email(message = "Provide a valid email address")
     @NotBlank(message = "This field cannot be blank")
+    @Column(unique = true)
     private String username;
     @NotBlank(message = "This field cannot be blank")
     @Length(min=8, max=20)
     private String password;
     @NotBlank(message = "This field cannot be blank")
+//    @Pattern(regexp = "^[0-9]+$", message = "Only numbers are allowed.")
+    @Pattern(regexp = "[0-9]{11}", message = "Only numbers are allowed.")
+    @Size(min = 11, max = 11, message = "Field length must be exactly 11 characters")
     private String phoneNumber;
+
+    public AccountUser() {
+    }
+
+    public AccountUser(String firstName, String middleName, String lastName, String username, String password, String phoneNumber) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+    }
 
     public String getFirstName() {
         return firstName;
@@ -87,4 +106,6 @@ public class AccountUser extends RepresentationModel<AccountUser> {
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
+
+
 }
